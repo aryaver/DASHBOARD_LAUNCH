813e3f5bd04ecb0fcb624ccb4657c313d4dacfd1
aryaver <arya.verma.923@gmail.com>
2023-11-07 14:40:24 +0530

    added relevant comments

--- first_launch.py
+++ first_launch.py
@@ -17,6 +17,7 @@ server = app.server
 # Define the layout of the app
 app.layout = html.Div(children = 
 [
+    # page title
     dbc.Row([
         dbc.Col([]),
         dbc.Col([html.H1("Clevered HR Dashboard", className="fw-bold text-decoration-underline", style={'color': 'black'})]),
@@ -24,16 +25,18 @@ app.layout = html.Div(children =
     
     ]),
     html.Br(),
+
+    # department radio buttons
     html.Div([
         dbc.Label("Select Department", className = "text-black fw-bold"),
         dbc.RadioItems(
             options=[
                 {"label": "Sales", "value": 0},
-                {"label": "Marketing", "value": 1},
-                {"label": "Support", "value": 2},
-                {"label": "Partnership", "value": 3},
-                {"label": "Trainers", "value": 4},
-                {"label": "IT", "value": 5},
+                {"label": "IT", "value": 1},
+                {"label": "Marketing", "value": 2},
+                {"label": "Support", "value": 3},
+                {"label": "Partnership", "value": 4},
+                {"label": "Trainers", "value": 5},
                 {"label": "Freelancers", "value": 6},
                 {"label": "Internship", "value": 7},
             ],
@@ -44,7 +47,9 @@ app.layout = html.Div(children =
         ),
     ]
 ),        
-    html.Br(),                                                     
+    html.Br(),  
+    
+    # file upload container                                                   
     html.Div(id='upload-container', className='centered-container', children=[
             dcc.Upload(
                 id='upload-data',
@@ -63,6 +68,8 @@ app.layout = html.Div(children =
             ),
         ]),
     html.Br(),
+
+    # month and name dropdowns
     html.Div([  dbc.Select( id='month-dropdown',options=[{'label': 'January', 'value': '01'},
                                                          {'label': 'February', 'value': '02'},
                                                          {'label': 'March', 'value': '03'},
@@ -86,6 +93,8 @@ app.layout = html.Div(children =
     ], style={'display': 'flex'}), 
 
     html.Br(),
+
+    # manual search bar with search and clear buttons
     dbc.Row([
         dbc.Col([
             html.Div([
@@ -103,14 +112,17 @@ app.layout = html.Div(children =
             ])
         ]),
              
+    # manual search output div
     html.Div(id='output-columns'),
     
     html.Div(id='output-person-info', style={'color': 'black'}),
     
+    # current month info div
     html.Div(id='current-month-info', style={'color': 'black'}),#, className = 'text-dark'),  
 
-    html.Div(id='current-month-bday-anni-info'), #to send bday anni info
- 
+    # email status div
+    html.Div(id='current-month-bday-anni-info'),
+
 ],style={
         'background': 'linear-gradient(360deg, #FF5733, #FFC300)',  
         'height': '200vh',  
@@ -124,11 +136,26 @@ def get_current_month():
     now = datetime.datetime.now()
     return now.strftime('%m')
 
-#Function to get current date
+#Function to get current date for bday/anni mail personalisation
 def get_current_date():
     now = datetime.datetime.now()
     return now.strftime('%d')
 
+# Function to extract month
+# def extract_month(date_str):
+#     try:
+#         date_str = date_str.strftime('%d%m%Y')  # Convert the datetime object to a string
+#         date = datetime.datetime.strptime(date_str, '%d%m%Y')
+#         return date.month
+#     except ValueError:
+#         return None
+# def extract_month(date_str):
+#     try:
+#         day, month, year = map(int, date_str.split('/'))
+#         return month
+#     except (ValueError, AttributeError):
+#         return None
+    
 def read_file(contents, filename, Sheet):
     if contents is None:
         return ''
@@ -143,8 +170,18 @@ def read_file(contents, filename, Sheet):
         raise ValueError(f"Unsupported File Format: {file_extension}")
 
     
-    df['date of birth'] = pd.to_datetime(df['date of birth'])
-    df['date of joining'] = pd.to_datetime(df['date of joining'])
+    # df['date of birth'] = pd.to_datetime(df['date of birth'], format='%d/%m/%Y', dayfirst=True)
+    # df['date of joining'] = pd.to_datetime(df['date of joining'], format='%d/%m/%Y', dayfirst=True)
+
+    # df['date of birth'] = df['date of birth'].apply(extract_month)
+    # df['date of joining'] = df['date of joining'].apply(extract_month)
+    # df['date of birth'] = df['date of birth'].str.split('/').str[1].astype(int)
+    # df['date of joining'] = df['date of joining'].str.split('/').str[1].astype(int)
+    # df['date of birth'] = pd.to_datetime(df['date of birth'], format='%d/%m/%Y').dt.month
+    # df['date of joining'] = pd.to_datetime(df['date of joining'], format='%d/%m/%Y').dt.month
+
+    df['birth_month'] = df['date of birth'].dt.month # extract birth month from sheet
+    df['joining_month'] = df['date of joining'].dt.month    
     # print("--------------------------------------------------------print all heads---------------------------------------------------------------")
     # print(df.head())
     
@@ -182,7 +219,7 @@ def send_email(sender_email, password, recv_email, subject, message):
         return f"Error: An unexpected error occurred - {str(e)}"
 
 
-#Function with email body
+# Function with personalised email body
 def email_body(bdays_today, annis_today):   
     body = ''  
     if bdays_today is not None and not bdays_today.empty:   
@@ -215,19 +252,29 @@ def display_current_month_info(contents, dept,  filename):
 
     df = read_file(contents, filename, dept)
     #print(df.columns)
+    # df['date of birth'] = df['date of birth'].apply(extract_month)
+    # df['date of joining'] = df['date of joining'].apply(extract_month)
 
     current_month = get_current_month()
-    birth_month_filter = df['date of birth'].dt.strftime('%m') == current_month  
+    birth_month_filter = df['date of birth'].dt.strftime('%m') == current_month  # make a df of records for which birth month = current month 
     joining_month_filter = df['date of joining'].dt.strftime('%m') == current_month
 
+    # birth_month_filter = df['date of birth'] == current_month  
+    # joining_month_filter = df['date of joining'] == current_month
+    # print(birth_month_filter)
+    
+    # birth_month_filter = df['date of birth'].dt.month == current_month 
+    # joining_month_filter = df['date of joining'].dt.month == current_month 
    
     birth_persons = df[birth_month_filter]
     joining_persons = df[joining_month_filter]
 
+    # initialize empty divs to display details
     birth_person_info_divs = []
     joining_person_info_divs = []
 
 
+    # for all names in the filtered dataframe, append them to the children component of person_info_div
     for _, row in birth_persons.iterrows():
         person_name = row['name']
         person_info_div = html.Div([
@@ -242,6 +289,7 @@ def display_current_month_info(contents, dept,  filename):
 
         birth_person_info_divs.append(person_info_div)
    
+    # similarly for work anniversary
     for _, row in joining_persons.iterrows():
         person_name = row['name']
         person_info_div = html.Div([
@@ -257,23 +305,24 @@ def display_current_month_info(contents, dept,  filename):
 
         joining_person_info_divs.append(person_info_div)
 
+    # add these two divs within current_month_info_div to display in one row on the output window
     current_month_info_div = html.Div([
         html.H2("Current Month Analysis", className="text-center fw-bold"),# fst-italic text-decoration-line-through"),
 
         html.Div([
             dbc.Row(
                     [
-                        dbc.Col(html.Div([html.H4('Birthdays in this month', className = "fst-italic fw-bold"),#, style={'width': '50%', 'margin': 'auto', 'text-align': 'center', 'font-size': '20px'}), 
-                                          html.P(birth_person_info_divs, style={'text-align': 'justify'})])), #, className='col-md-6')]),),
-                        dbc.Col(html.Div([html.H4('Work Anniversaries in this month', className = "fst-italic fw-bold"),#, style={'width': '50%', 'margin': 'auto', 'text-align': 'center', 'font-size': '20px'}), 
+                        dbc.Col(html.Div([html.H4('Birthdays in this month', className = "fst-italic fw-bold"),
+                                          html.P(birth_person_info_divs, style={'text-align': 'justify'})])), 
+                        dbc.Col(html.Div([html.H4('Work Anniversaries in this month', className = "fst-italic fw-bold"),
                                           html.P(joining_person_info_divs, style={'text-align': 'justify'})])),
                     ]
                 ),
             ])])
  
-    return current_month_info_div
+    return current_month_info_div # return this div as output. it gets vreated only when a valid file is updated. else this division does not exist
 
-#combined callback to handle person info display, name dropdown updates, and manual search
+# combined callback to handle person info display, name dropdown updates, and manual search
 @app.callback(
     [
         Output('output-person-info', 'children'),
@@ -302,9 +351,12 @@ def update_person_info(contents, filename, dept, selected_name, selected_month, 
 
     if selected_month:
         current_month = selected_month
+
+        # filters to get records with bdays and annis in the particular month
         birth_month_filter = df['date of birth'].dt.strftime('%m') == current_month
         joining_month_filter = df['date of joining'].dt.strftime('%m') == current_month
 
+        # join records from both filters
         filtered_persons = df[birth_month_filter | joining_month_filter]
 
         person_info_divs = []
@@ -330,16 +382,17 @@ def update_person_info(contents, filename, dept, selected_name, selected_month, 
             if person_info_div.children[0].children == f'Details for {selected_name}:':
                 output_person_info = person_info_div
                 break
-    person_info = pd.DataFrame() #
+    person_info = pd.DataFrame() 
 
-    if search_clicks is not None and search_clicks > 0 and manual_search_name:
-        manual_search_name_components = manual_search_name.split(' ')
-        manual_search_name_first = manual_search_name_components[0].lower()
+    if search_clicks is not None and search_clicks > 0 and manual_search_name: # if a name is enteres and the search button is clicked
+        manual_search_name_components = manual_search_name.split(' ') # se delimiter " " to separate parts of the name and extract the first and last name
+        manual_search_name_first = manual_search_name_components[0].lower() # manual_search_name_components[0] gives the first name and .lower90 to convert to lower case for case-insensitive search
         df['name_lower'] = df['name'].str.lower()
 
         person_info = df[df['name_lower'].str.startswith(manual_search_name_first)].copy() ##create a copy of the original df
         person_info.drop('name_lower', axis = 1, inplace = True) #inplace true for dropping col from the same df and not making a copy of the dataframe with a col dropped. axis = 1 to delete col and not row(axis=0)
         
+
     if not person_info.empty:
             person_info_dict = person_info.iloc[0].to_dict()
             output_person_info = html.Div([
@@ -351,8 +404,10 @@ def update_person_info(contents, filename, dept, selected_name, selected_month, 
 
             name_dropdown_options.append({'label': manual_search_name, 'value': manual_search_name})
 
+    # if clear is pressed, return empty string 
     if clear_clicks is not None and clear_clicks >0:
             manual_search_name = ''
+
     return output_person_info, name_dropdown_options, manual_search_name
 
 @app.callback(
@@ -364,6 +419,8 @@ def update_person_info(contents, filename, dept, selected_name, selected_month, 
     State('upload-data', 'filename')
 )
 def send_bday_anni_info(contents, dept, n_clicks, password, filename):
+
+    # if a file is uploaded and the password is entered and send button is pressed:
     if n_clicks > 0 and contents is not None and password is not None:
 
         df = read_file(contents, filename, dept)
@@ -373,16 +430,17 @@ def send_bday_anni_info(contents, dept, n_clicks, password, filename):
 
         current_month = get_current_month()
         current_date = get_current_date()
-        birth_month_filter = (df['date of birth'].dt.strftime('%m') == current_month) & (df['date of birth'].dt.strftime('%d') == current_date)
-        bdays_today = df[birth_month_filter][['name', 'email ID']] 
+
+        birth_month_filter = (df['date of birth'].dt.strftime('%m') == current_month) & (df['date of birth'].dt.strftime('%d') == current_date) # extract employee records that have birthday on the current day
+        bdays_today = df[birth_month_filter][['name', 'email ID']] # append to bdays_today if birthday is today
 
         anni_month_filter = (df['date of joining'].dt.strftime('%m') == current_month) & (df['date of joining'].dt.strftime('%d') == current_date)
         annis_today = df[anni_month_filter][['name', 'email ID']] 
 
-        message = email_body(bdays_today, annis_today)
+        message = email_body(bdays_today, annis_today) # create customised message
      
-        if len(bdays_today) > 0 or len(annis_today) > 0:
-            send_email(sender_email, password, recipient_email, "Birthdays and Work Anniversaries today.", message)   
+        if len(bdays_today) > 0 or len(annis_today) > 0: # check if any employee has bday/anni on the present day
+            send_email(sender_email, password, recipient_email, "Birthdays and Work Anniversaries today.", message) # send mail  
             if send_email(sender_email, password, recipient_email, "Birthdays and Work Anniversaries today.", message) == 1:
                 return dbc.Alert('Email sent successfully!', color = 'success', style = {'width':'30vw'}) 
             else:
